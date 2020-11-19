a2b7b46a520d3aa85c4b6f18cc85617527258953
William Thorenfeldt <wrt_95@hotmail.com>
2020-11-19 12:57:31 +0000

    Updates

---
+++ precache-manifest.f1e4d43494998e035c85b9cbca628cd8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e865ba6e733b6c29b2e6a3ab0d0cb0",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "62bd879f65ae0791ac0d",
+    "url": "/home/static/css/main.4e7055c8.chunk.css"
+  },
+  {
+    "revision": "a27358596a80fde41c9b",
+    "url": "/home/static/js/2.6621a217.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/home/static/js/2.6621a217.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62bd879f65ae0791ac0d",
+    "url": "/home/static/js/main.fdda3c5d.chunk.js"
+  },
+  {
+    "revision": "74ec90099619b48a8cd0",
+    "url": "/home/static/js/runtime-main.6347e3b0.js"
+  },
+  {
+    "revision": "18aed54f48908bacf82235c9f82bdd24",
+    "url": "/home/static/media/AppLogo.18aed54f.png"
+  },
+  {
+    "revision": "7a056dd1e9b2b5e4b1fcea651c2de1fe",
+    "url": "/home/static/media/companyLogo.7a056dd1.png"
+  }
+]);
